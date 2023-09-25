d886ae958fd04826673d9646dfb6500c97067c92
Andrea De Rinaldis <andrea.derinaldis@pagopa.it>
2023-09-25 08:55:24 +0200

    [NOD-484] fix: refactored persistence configurator class

--- src/main/java/it/gov/pagopa/apiconfig/datamigration/config/PersistenceConfiguration.java
+++ src/main/java/it/gov/pagopa/apiconfig/datamigration/config/PersistenceConfiguration.java
@@ -1,4 +1,4 @@
-package it.gov.pagopa.apiconfig.datamigration.util;
+package it.gov.pagopa.apiconfig.datamigration.config;
 
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.context.properties.ConfigurationProperties;
@@ -12,18 +12,18 @@ import org.springframework.transaction.PlatformTransactionManager;
 import javax.sql.DataSource;
 
 @Configuration
-public class PersistenceAutoConfiguration {
+public class PersistenceConfiguration {
 
     @Primary
     @Bean(name = "oracleDataSource")
-    @ConfigurationProperties(prefix = "spring.datasource")
-    public DataSource firstDataSource() {
+    @ConfigurationProperties(prefix = "spring.oracledb-datasource")
+    public DataSource oracleDataSource() {
         return DataSourceBuilder.create().build();
     }
 
     @Bean(name = "postgresDataSource")
-    @ConfigurationProperties(prefix = "spring.second-datasource")
-    public DataSource secondDataSource() {
+    @ConfigurationProperties(prefix = "spring.postgresql-datasource")
+    public DataSource postgresDataSource() {
         return DataSourceBuilder.create().build();
     }
 
