54c040d7ee6d7507f3302ed9c3b02924acbc1850
Andrea De Rinaldis <andrea.derinaldis@pagopa.it>
2023-09-25 19:18:15 +0200

    [NOD-484] feat: removed unused table

--- src/main/java/it/gov/pagopa/apiconfig/datamigration/repository/OracleRepository.java
+++ src/main/java/it/gov/pagopa/apiconfig/datamigration/repository/OracleRepository.java
@@ -1,6 +1,5 @@
 package it.gov.pagopa.apiconfig.datamigration.repository;
 
-import it.gov.pagopa.apiconfig.datamigration.model.Table;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
