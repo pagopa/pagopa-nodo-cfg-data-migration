94c6ba561ff7bfa32b71010981bf45bd84fa8e5d
Andrea De Rinaldis <andrea.derinaldis@pagopa.it>
2023-09-25 19:16:16 +0200

    [NOD-484] feat: added enhancement for START step

--- src/main/java/it/gov/pagopa/apiconfig/datamigration/fsm/step/StartStep.java
+++ src/main/java/it/gov/pagopa/apiconfig/datamigration/fsm/step/StartStep.java
@@ -1,6 +1,8 @@
 package it.gov.pagopa.apiconfig.datamigration.fsm.step;
 
 import it.gov.pagopa.apiconfig.datamigration.entity.DataMigration;
+import it.gov.pagopa.apiconfig.datamigration.entity.DataMigrationDetails;
+import it.gov.pagopa.apiconfig.datamigration.entity.DataMigrationStatus;
 import it.gov.pagopa.apiconfig.datamigration.enumeration.MigrationStepStatus;
 import it.gov.pagopa.apiconfig.datamigration.exception.migration.MigrationStatusSavingException;
 import it.gov.pagopa.apiconfig.datamigration.exception.migration.MigrationStepException;
@@ -8,8 +10,11 @@ import it.gov.pagopa.apiconfig.datamigration.enumeration.StepName;
 import it.gov.pagopa.apiconfig.datamigration.exception.migration.MigrationTruncateAllTablesException;
 import it.gov.pagopa.apiconfig.datamigration.fsm.Step;
 import it.gov.pagopa.apiconfig.datamigration.repository.CfgDataMigrationRepository;
+import it.gov.pagopa.apiconfig.datamigration.util.CommonUtils;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
 
 import java.sql.Timestamp;
@@ -20,27 +25,30 @@ import java.util.UUID;
 @Service("START")
 public class StartStep extends Step {
 
-    //@Autowired
-    private CfgDataMigrationRepository repository;
+    @Autowired
+    private CfgDataMigrationRepository cfgDataMigrationRepo;
 
     @Override
     public void executeStep() throws MigrationStepException {
-
         // resetting flags and creating a new record in the CFG_DATA_MIGRATION table
         activateMigration();
-
+        // deleting all data in all tables
         truncateAllTables();
-
     }
 
     @Override
     public StepName getNextState() {
-        return StepName.EXECUTE_GENERIC_TABLE_MIGRATION;
+        return StepName.EXECUTE_INTERMEDIARIPA_TABLE_MIGRATION;
     }
 
     @Override
     public String getStepName() {
         return StepName.START.toString();
+    }
+
+    @Override
+    public DataMigrationStatus getDataMigrationStatus(DataMigrationDetails details) {
+        return null;
     }
 
     private void activateMigration() throws MigrationStatusSavingException {
@@ -51,11 +59,50 @@ public class StartStep extends Step {
         try {
             DataMigration dataMigration = DataMigration.builder()
                     .id(UUID.randomUUID().toString())
-                    .start(Timestamp.valueOf(LocalDateTime.now()))
+                    .start(CommonUtils.now())
                     .status(MigrationStepStatus.IN_PROGRESS.toString())
-                    .details(null) // TODO add generation of the DataMigrationDetail object
+                    .details(DataMigrationDetails.builder()
+                            .lastExecutedStep(StepName.START.toString())
+                            .intermediariPa(new DataMigrationStatus())
+                            .pa(new DataMigrationStatus())
+                            .stazioni(new DataMigrationStatus())
+                            .paStazioniPa(new DataMigrationStatus())
+                            .codifiche(new DataMigrationStatus())
+                            .codifichePa(new DataMigrationStatus())
+                            .binaryFile(new DataMigrationStatus())
+                            .ibanValidiPerPa(new DataMigrationStatus())
+                            .informativeContoAccreditoMaster(new DataMigrationStatus())
+                            .informativeContoAccreditoDetail(new DataMigrationStatus())
+                            .informativePaMaster(new DataMigrationStatus())
+                            .informativePaDetail(new DataMigrationStatus())
+                            .informativePaFasce(new DataMigrationStatus())
+                            .intermediariPsp(new DataMigrationStatus())
+                            .psp(new DataMigrationStatus())
+                            .canaliNodo(new DataMigrationStatus())
+                            .canali(new DataMigrationStatus())
+                            .tipiVersamento(new DataMigrationStatus())
+                            .canaleTipoVersamento(new DataMigrationStatus())
+                            .pspCanaleTipoVersamento(new DataMigrationStatus())
+                            .dizionarioMetadati(new DataMigrationStatus())
+                            .cdiMaster(new DataMigrationStatus())
+                            .cdiDetail(new DataMigrationStatus())
+                            .cdiFasciaCostoServizio(new DataMigrationStatus())
+                            .cdiInformazioniServizio(new DataMigrationStatus())
+                            .cdiPreferences(new DataMigrationStatus())
+                            .elencoServizi(new DataMigrationStatus())
+                            .cdsCategorie(new DataMigrationStatus())
+                            .cdsSoggetto(new DataMigrationStatus())
+                            .cdsServizio(new DataMigrationStatus())
+                            .cdsSoggettoServizio(new DataMigrationStatus())
+                            .configurationKeys(new DataMigrationStatus())
+                            .wfespPluginConn(new DataMigrationStatus())
+                            .ftpServer(new DataMigrationStatus())
+                            .pdd(new DataMigrationStatus())
+                            .gdeConfig(new DataMigrationStatus())
+                            .quadratureSched(new DataMigrationStatus())
+                            .build())
                     .build();
-            //repository.save(dataMigration); // TODO set the correct mode for save data
+            // cfgDataMigrationRepo.save(dataMigration); // TODO de-comment when repository is created
         } catch (DataAccessException e) {
             throw new MigrationStatusSavingException(e);
         }
@@ -63,9 +110,22 @@ public class StartStep extends Step {
 
     private void truncateAllTables() throws MigrationTruncateAllTablesException {
         try {
-            // TODO set the correct mode for truncate all tables
+            // deleteAndFlush(quadratureSchedRepo)
+            // deleteAndFlush(gdeConfigRepo);
+            // deleteAndFlush(pddRepo);
+            // deleteAndFlush(ftpServerRepo);
+            // deleteAndFlush(wfespPluginConnRepo);
+            // deleteAndFlush(configurationKeysRepo);
+            // deleteAndFlush(cdsSoggettoServizioRepo);
+            // deleteAndFlush(cdsServizioRepo);
+            // TODO continue with other tables
         } catch (DataAccessException e) {
             throw new MigrationTruncateAllTablesException(e);
         }
+    }
+
+    private void deleteAndFlush(JpaRepository repo) {
+        repo.deleteAll();
+        repo.flush();
     }
 }
