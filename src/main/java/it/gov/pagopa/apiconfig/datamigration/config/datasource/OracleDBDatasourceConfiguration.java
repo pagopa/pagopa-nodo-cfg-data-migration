3cf44242d59d469849ed8188d6bafce5ad6c27b9
Andrea De Rinaldis <andrea.derinaldis@pagopa.it>
2023-09-28 09:18:57 +0200

    [NOD-484] fix: reverted changes on OracleDBDatasourceConfiguration

--- src/main/java/it/gov/pagopa/apiconfig/datamigration/config/datasource/OracleDBDatasourceConfiguration.java
+++ src/main/java/it/gov/pagopa/apiconfig/datamigration/config/datasource/OracleDBDatasourceConfiguration.java
@@ -13,7 +13,6 @@ import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
@@ -58,7 +57,7 @@ public class OracleDBDatasourceConfiguration {
     }
 
     @Primary
-    @Bean(name = "transactionManager")
+    @Bean(name = "oracledbTransactionManager")
     public PlatformTransactionManager transactionManager (
             @Qualifier("oracleEntityManagerFactory")EntityManagerFactory entityManagerFactory) {
         return new JpaTransactionManager(entityManagerFactory);
