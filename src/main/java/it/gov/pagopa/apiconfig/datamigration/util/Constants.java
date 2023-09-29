34a9f9730d5d793670342801d64e7c8859578398
Andrea De Rinaldis <andrea.derinaldis@pagopa.it>
2023-09-29 17:11:23 +0200

    [NOD-484] fix: resolved typo on message

--- src/main/java/it/gov/pagopa/apiconfig/datamigration/util/Constants.java
+++ src/main/java/it/gov/pagopa/apiconfig/datamigration/util/Constants.java
@@ -9,7 +9,7 @@ import java.util.Set;
 public class Constants {
 
     public static final String API_START_OK = "Migration started successfully.";
-    public static final String API_STOP_OK = "he migration will be terminated gracefully. Please, wait for the termination and saving of the state.";
+    public static final String API_STOP_OK = "The migration will be terminated gracefully. Please, wait for the termination and saving of the state.";
     public static final String API_RESTART_OK = "Migration restarted successfully.";
     public static final String STATUS_CONFLICT = "Status Conflict";
 
