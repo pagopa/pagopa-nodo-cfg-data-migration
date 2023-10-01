ddd3d914ba0c436796b4ec5d45dc2686c33f962a
Andrea De Rinaldis <andrea.derinaldis@pagopa.it>
2023-10-01 15:48:59 +0200

    [NOD-507] fix: updated Unit tests

--- src/test/java/it/gov/pagopa/apiconfig/datamigration/ApplicationTest.java
+++ src/test/java/it/gov/pagopa/apiconfig/datamigration/ApplicationTest.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-/*
+
 @SpringBootTest
 class ApplicationTest {
 
@@ -15,4 +15,3 @@ class ApplicationTest {
     assertTrue(true);
   }
 }
-*/
