ec27d769abee33f7aebf6a0d743ed2917b26366f
GiampieroPorfiri <giampiero.porfiri@tasgroup.eu>
2023-10-04 10:55:55 +0200

    [NOD-506] feat: Updated OpenApiConfig and added dependency to pom

--- src/test/java/it/gov/pagopa/nodo/datamigration/OpenApiGenerationTest.java
+++ src/test/java/it/gov/pagopa/nodo/datamigration/OpenApiGenerationTest.java
@@ -1,28 +1,29 @@
 package it.gov.pagopa.nodo.datamigration;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-
 import com.fasterxml.jackson.databind.ObjectMapper;
-
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import javax.sql.DataSource;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
-@SpringBootTest(classes = Application.class)
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
 class OpenApiGenerationTest {
 
@@ -42,22 +43,22 @@ class OpenApiGenerationTest {
 
     @Test
     void swaggerSpringPlugin() throws Exception {
-//        mvc.perform(MockMvcRequestBuilders.get("/v3/api-docs").accept(MediaType.APPLICATION_JSON))
-//                .andExpect(MockMvcResultMatchers.status().is2xxSuccessful())
-//                .andDo(
-//                        (result) -> {
-//                            assertNotNull(result);
-//                            assertNotNull(result.getResponse());
-//                            final String content = result.getResponse().getContentAsString();
-//                            assertFalse(content.isBlank());
-//                            assertFalse(content.contains("${"), "Generated swagger contains placeholders");
-//                            Object swagger =
-//                                    objectMapper.readValue(result.getResponse().getContentAsString(), Object.class);
-//                            String formatted =
-//                                    objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(swagger);
-//                            Path basePath = Paths.get("openapi/");
-//                            Files.createDirectories(basePath);
-//                            Files.write(basePath.resolve("openapi.json"), formatted.getBytes());
-//                        });
+        mvc.perform(MockMvcRequestBuilders.get("/v3/api-docs").accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().is2xxSuccessful())
+                .andDo(
+                        (result) -> {
+                            assertNotNull(result);
+                            assertNotNull(result.getResponse());
+                            final String content = result.getResponse().getContentAsString();
+                            assertFalse(content.isBlank());
+                            assertFalse(content.contains("${"), "Generated swagger contains placeholders");
+                            Object swagger =
+                                    objectMapper.readValue(result.getResponse().getContentAsString(), Object.class);
+                            String formatted =
+                                    objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(swagger);
+                            Path basePath = Paths.get("openapi/");
+                            Files.createDirectories(basePath);
+                            Files.write(basePath.resolve("openapi.json"), formatted.getBytes());
+                        });
     }
 }
