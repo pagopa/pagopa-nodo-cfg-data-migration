77b20bf7c7e192fb606e18d4979768d2a6151f45
Andrea De Rinaldis <andrea.derinaldis@pagopa.it>
2023-10-06 14:41:21 +0200

    [NOD-507] fix: changed deletion strategy

--- src/main/java/it/gov/pagopa/nodo/datamigration/fsm/step/StartStep.java
+++ src/main/java/it/gov/pagopa/nodo/datamigration/fsm/step/StartStep.java
@@ -14,8 +14,11 @@ import it.gov.pagopa.nodo.datamigration.repository.oracle.OracleDBSystemReposito
 import it.gov.pagopa.nodo.datamigration.repository.postgres.*;
 import it.gov.pagopa.nodo.datamigration.service.HealthCheckService;
 import it.gov.pagopa.nodo.datamigration.util.CommonUtils;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.dao.DataAccessException;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
@@ -26,42 +29,11 @@ import java.util.UUID;
 @Service("START")
 public class StartStep extends Step {
 
-    @Autowired private BinaryFileDestRepository binaryFileRepo;
-    @Autowired private CanaleTipoVersamentoDestRepository canaleTipoVersamentoRepo;
-    @Autowired private CanaliDestRepository canaliRepo;
-    @Autowired private CanaliNodoDestRepository canaliNodoRepo;
-    @Autowired private CdiDetailDestRepository cdiDetailRepo;
-    @Autowired private CdiFasciaCostoServizioDestRepository cdiFasciaCostoServizioRepo;
-    @Autowired private CdiInformazioniServizioDestRepository cdiInformazioniServizioRepo;
-    @Autowired private CdiMasterDestRepository cdiMasterRepo;
-    @Autowired private CdiPreferenceDestRepository cdiPreferenceRepo;
-    @Autowired private CdsCategoriaDestRepository cdsCategoriaRepo;
-    @Autowired private CdsServizioDestRepository cdsServizioRepo;
-    @Autowired private CdsSoggettoDestRepository cdsSoggettoRepo;
-    @Autowired private CdsSoggettoServizioDestRepository cdsSoggettoServizioRepo;
-    @Autowired private CodificheDestRepository codificheRepo;
-    @Autowired private CodifichePaDestRepository codifichePaRepo;
-    @Autowired private ConfigurationKeysDestRepository configurationKeysRepo;
-    @Autowired private DizionarioMetadatiDestRepository dizionarioMetadatiRepo;
-    @Autowired private ElencoServiziDestRepository elencoServiziRepo;
-    @Autowired private FtpServersDestRepository ftpServersRepo;
-    @Autowired private GdeConfigDestRepository gdeConfigRepo;
-    @Autowired private InformativeContoAccreditoDetailDestRepository informativeContoAccreditoDetailRepo;
-    @Autowired private InformativeContoAccreditoMasterDestRepository informativeContoAccreditoMasterRepo;
-    @Autowired private InformativePaDetailDestRepository informativePaDetailRepo;
-    @Autowired private InformativePaFasceDestRepository informativePaFasceRepo;
-    @Autowired private InformativePaMasterDestRepository informativePaMasterRepo;
-    @Autowired private IntermediariPaDestRepository intermediariPaRepo;
-    @Autowired private IntermediariPspDestRepository intermediariPspRepo;
-    @Autowired private PaDestRepository paRepo;
-    @Autowired private PaStazionePaDestRepository paStazionePaRepo;
-    @Autowired private PddDestRepository pddRepo;
-    @Autowired private PspCanaleTipoVersamentoDestRepository pspCanaleTipoVersamentoRepo;
-    @Autowired private PspDestRepository pspRepo;
-    @Autowired private QuadratureSchedDestRepository quadratureSchedRepo;
-    @Autowired private StazioniDestRepository stazioniRepo;
-    @Autowired private TipiVersamentoDestRepository tipiVersamentoRepo;
-    @Autowired private WfespPluginConfDestRepository wfespPluginConfRepo;
+    @PersistenceContext(unitName="postgresqlUnit")
+    private EntityManager destEM;
+
+    @Value("${persistence.postgresql.default_schema}")
+    private String schema;
 
     @Autowired private HealthCheckService healthCheckService;
 
@@ -156,85 +128,87 @@ public class StartStep extends Step {
     private void truncateAllTables() throws MigrationTruncateAllTablesException {
         try {
             log.info(" - Starting deleting all data from GDE_CONFIG...");
-            deleteAndFlush(gdeConfigRepo);
+            deleteAndFlush("GDE_CONFIG");
             log.info(" - Deleted all data from GDE_CONFIG. Starting deleting all data from PDD...");
-            deleteAndFlush(pddRepo);
+            deleteAndFlush("PDD");
             log.info(" - Deleted all data from PDD. Starting deleting all data from FTP_SERVER...");
-            deleteAndFlush(ftpServersRepo);
+            deleteAndFlush("FTP_SERVER");
             log.info(" - Deleted all data from FTP_SERVER. Starting deleting all data from CONFIGURATION_KEYS...");
-            deleteAndFlush(configurationKeysRepo);
+            deleteAndFlush("CONFIGURATION_KEYS");
             log.info(" - Deleted all data from CONFIGURATION_KEYS. Starting deleting all data from CDS_SOGGETTO_SERVIZIO...");
-            deleteAndFlush(cdsSoggettoServizioRepo);
+            deleteAndFlush("CDS_SOGGETTO_SERVIZIO");
             log.info(" - Deleted all data from CDS_SOGGETTO_SERVIZIO. Starting deleting all data from CDS_SERVIZIO...");
-            deleteAndFlush(cdsServizioRepo);
+            deleteAndFlush("CDS_SERVIZIO");
             log.info(" - Deleted all data from CDS_SERVIZIO. Starting deleting all data from CDS_SOGGETTO...");
-            deleteAndFlush(cdsSoggettoRepo);
+            deleteAndFlush("CDS_SOGGETTO");
             log.info(" - Deleted all data from CDS_SOGGETTO. Starting deleting all data from CDS_CATEGORIA...");
-            deleteAndFlush(cdsCategoriaRepo);
+            deleteAndFlush("CDS_CATEGORIA");
             log.info(" - Deleted all data from CDS_CATEGORIA. Starting deleting all data from ELENCO_SERVIZI...");
-            deleteAndFlush(elencoServiziRepo);
+            deleteAndFlush("ELENCO_SERVIZI");
             log.info(" - Deleted all data from ELENCO_SERVIZI. Starting deleting all data from CDI_PREFERENCES...");
-            deleteAndFlush(cdiPreferenceRepo);
+            deleteAndFlush("CDI_PREFERENCES");
             log.info(" - Deleted all data from CDI_PREFERENCES. Starting deleting all data from CDI_INFORMAZIONI_SERVIZIO...");
-            deleteAndFlush(cdiInformazioniServizioRepo);
+            deleteAndFlush("CDI_INFORMAZIONI_SERVIZIO");
             log.info(" - Deleted all data from CDI_INFORMAZIONI_SERVIZIO. Starting deleting all data from CDI_FASCIA_COSTO_SERVIZIO...");
-            deleteAndFlush(cdiFasciaCostoServizioRepo);
+            deleteAndFlush("CDI_FASCIA_COSTO_SERVIZIO");
             log.info(" - Deleted all data from CDI_FASCIA_COSTO_SERVIZIO. Starting deleting all data from CDI_DETAIL...");
-            deleteAndFlush(cdiDetailRepo);
+            deleteAndFlush("CDI_DETAIL");
             log.info(" - Deleted all data from CDI_DETAIL. Starting deleting all data from CDI_MASTER...");
-            deleteAndFlush(cdiMasterRepo);
+            deleteAndFlush("CDI_MASTER");
             log.info(" - Deleted all data from CDI_MASTER. Starting deleting all data from DIZIONARIO_METADATI...");
-            deleteAndFlush(dizionarioMetadatiRepo);
+            deleteAndFlush("DIZIONARIO_METADATI");
             log.info(" - Deleted all data from DIZIONARIO_METADATI. Starting deleting all data from PSP_CANALE_TIPO_VERSAMENTO...");
-            deleteAndFlush(pspCanaleTipoVersamentoRepo);
+            deleteAndFlush("PSP_CANALE_TIPO_VERSAMENTO");
             log.info(" - Deleted all data from PSP_CANALE_TIPO_VERSAMENTO. Starting deleting all data from CANALE_TIPO_VERSAMENTO...");
-            deleteAndFlush(canaleTipoVersamentoRepo);
+            deleteAndFlush("CANALE_TIPO_VERSAMENTO");
             log.info(" - Deleted all data from CANALE_TIPO_VERSAMENTO. Starting deleting all data from TIPI_VERSAMENTO...");
-            deleteAndFlush(tipiVersamentoRepo);
+            deleteAndFlush("TIPI_VERSAMENTO");
             log.info(" - Deleted all data from TIPI_VERSAMENTO. Starting deleting all data from CANALI_REPO...");
-            deleteAndFlush(canaliRepo);
+            deleteAndFlush("CANALI_REPO");
             log.info(" - Deleted all data from CANALI_REPO. Starting deleting all data from CANALI_NODO...");
-            deleteAndFlush(canaliNodoRepo);
+            deleteAndFlush("CANALI_NODO");
             log.info(" - Deleted all data from CANALI_NODO. Starting deleting all data from WFESP_PLUGIN_CONF...");
-            deleteAndFlush(wfespPluginConfRepo);
+            deleteAndFlush("WFESP_PLUGIN_CONF");
             log.info(" - Deleted all data from WFESP_PLUGIN_CONF. Starting deleting all data from PSP...");
-            deleteAndFlush(pspRepo);
+            deleteAndFlush("PSP");
             log.info(" - Deleted all data from PSP. Starting deleting all data from INTERMEDIARI_PSP...");
-            deleteAndFlush(intermediariPspRepo);
+            deleteAndFlush("INTERMEDIARI_PSP");
             log.info(" - Deleted all data from INTERMEDIARI_PSP. Starting deleting all data from INFORMATIVE_PA_FASCE...");
-            deleteAndFlush(informativePaFasceRepo);
+            deleteAndFlush("INFORMATIVE_PA_FASCE");
             log.info(" - Deleted all data from INFORMATIVE_PA_FASCE. Starting deleting all data from INFORMATIVE_PA_DETAIL...");
-            deleteAndFlush(informativePaDetailRepo);
+            deleteAndFlush("INFORMATIVE_PA_DETAIL");
             log.info(" - Deleted all data from INFORMATIVE_PA_DETAIL. Starting deleting all data from INFORMATIVE_PA_MASTER...");
-            deleteAndFlush(informativePaMasterRepo);
+            deleteAndFlush("INFORMATIVE_PA_MASTER");
             log.info(" - Deleted all data from INFORMATIVE_PA_MASTER. Starting deleting all data from INFORMATIVE_CONTO_ACCREDITO_DETAIL...");
-            deleteAndFlush(informativeContoAccreditoDetailRepo);
+            deleteAndFlush("INFORMATIVE_CONTO_ACCREDITO_DETAIL");
             log.info(" - Deleted all data from INFORMATIVE_CONTO_ACCREDITO_DETAIL. Starting deleting all data from INFORMATIVE_CONTO_ACCREDITO_MASTER...");
-            deleteAndFlush(informativeContoAccreditoMasterRepo);
+            deleteAndFlush("INFORMATIVE_CONTO_ACCREDITO_MASTER");
             log.info(" - Deleted all data from INFORMATIVE_CONTO_ACCREDITO_MASTER. Starting deleting all data from BINARY_FILE...");
-            deleteAndFlush(binaryFileRepo);
+            deleteAndFlush("BINARY_FILE");
             log.info(" - Deleted all data from BINARY_FILE. Starting deleting all data from CODIFICHE_PA...");
-            deleteAndFlush(codifichePaRepo);
+            deleteAndFlush("CODIFICHE_PA");
             log.info(" - Deleted all data from CODIFICHE_PA. Starting deleting all data from CODIFICHE...");
-            deleteAndFlush(codificheRepo);
+            deleteAndFlush("CODIFICHE");
             log.info(" - Deleted all data from CODIFICHE. Starting deleting all data from PA_STAZIONE_PA...");
-            deleteAndFlush(paStazionePaRepo);
+            deleteAndFlush("PA_STAZIONE_PA");
             log.info(" - Deleted all data from PA_STAZIONE_PA. Starting deleting all data from STAZIONI...");
-            deleteAndFlush(stazioniRepo);
+            deleteAndFlush("STAZIONI");
             log.info(" - Deleted all data from STAZIONI. Starting deleting all data from PA...");
-            deleteAndFlush(paRepo);
+            deleteAndFlush("PA");
             log.info(" - Deleted all data from PA. Starting deleting all data from INTERMEDIARI_PA...");
-            deleteAndFlush(intermediariPaRepo);
+            deleteAndFlush("INTERMEDIARI_PA");
             log.info(" - Deleted all data from INTERMEDIARI_PA. Starting deleting all data from QUADRATURE_SCHED...");
-            deleteAndFlush(quadratureSchedRepo);
+            deleteAndFlush("QUADRATURE_SCHED");
             log.info(" - Deleted all data from QUADRATURE_SCHED. Ended deleting all previous data!.");
         } catch (DataAccessException e) {
             throw new MigrationTruncateAllTablesException(e);
         }
     }
 
-    private void deleteAndFlush(JpaRepository repo) {
-        repo.deleteAll();
-        repo.flush();
+    private void deleteAndFlush(String table) {
+        destEM.createNativeQuery(String.format("DELETE FROM %s.%s", schema, table))
+                .executeUpdate();
+        destEM.flush();
+        destEM.clear();
     }
 }
