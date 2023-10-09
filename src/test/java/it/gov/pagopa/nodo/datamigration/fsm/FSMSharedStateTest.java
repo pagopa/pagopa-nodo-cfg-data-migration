9bb4e02b4f5c47ea7563bf5136e1e08c28d6f80e
GiampieroPorfiri <giampiero.porfiri@tasgroup.eu>
2023-10-09 12:02:21 +0200

    [NOD-506] feat: FSMSharedState.java test cases have been implemented

---
+++ src/test/java/it/gov/pagopa/nodo/datamigration/fsm/FSMSharedStateTest.java
@@ -0,0 +1,51 @@
+package it.gov.pagopa.nodo.datamigration.fsm;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+@SpringBootTest(classes = FSMSharedState.class)
+class FSMSharedStateTest {
+
+    private FSMSharedState fsmSharedState;
+
+    @BeforeEach
+    void setUp() {
+        fsmSharedState = new FSMSharedState();
+    }
+
+    @Test
+    void testRequestBlock() {
+        assertFalse(fsmSharedState.isBlockRequested());
+        fsmSharedState.requestBlock();
+        assertTrue(fsmSharedState.isBlockRequested());
+    }
+
+    @Test
+    void testLockAndUnlock() {
+        assertFalse(fsmSharedState.isInLock());
+        fsmSharedState.lock();
+        assertTrue(fsmSharedState.isInLock());
+        fsmSharedState.unlock();
+        assertFalse(fsmSharedState.isInLock());
+    }
+
+    @Test
+    void testResetStates() {
+        fsmSharedState.setDataMigrationStateId("TestID");
+        fsmSharedState.requestBlock();
+        fsmSharedState.lock();
+
+        fsmSharedState.resetStates();
+
+        assertFalse(fsmSharedState.isBlockRequested());
+        assertTrue(fsmSharedState.isInLock());
+        assertNull(fsmSharedState.getDataMigrationStateId());
+    }
+
+}
