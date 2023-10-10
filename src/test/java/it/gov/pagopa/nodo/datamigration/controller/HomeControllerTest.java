56d6c9159f01525cf16fd6c82380ad79f6f1ad30
GiampieroPorfiri <giampiero.porfiri@tasgroup.eu>
2023-10-10 21:37:59 +0200

    [NOD-506] feat: Implemented test cases for the controllers

---
+++ src/test/java/it/gov/pagopa/nodo/datamigration/controller/HomeControllerTest.java
@@ -0,0 +1,36 @@
+package it.gov.pagopa.nodo.datamigration.controller;
+
+import it.gov.pagopa.nodo.datamigration.service.HealthCheckService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+@WebMvcTest(HomeController.class)
+class HomeControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private HealthCheckService healthCheckService;
+
+    @Test
+    public void testHealthCheckEndpoint() throws Exception {
+        when(healthCheckService.getHealthCheckForOracleDB()).thenReturn(true);
+        when(healthCheckService.getHealthCheckForPostgresDB()).thenReturn(true);
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/info"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON));
+    }
+}
